3f3bc168a64c17fb903acaeab9f8192432c054d0
mitcool <dimitaraleksiev4@gmail.com>
2023-10-12 13:39:51 +0300

    Single category show and category resource created

--- app/Http/Controllers/Api/CategoriesController.php
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Http\Resources\CategoryResource;
 
 class CategoriesController extends Controller
 {
     public function index(){
-        return Category::all();
+        $categories =  Category::all();
+        return CategoryResource::collection($categories);
+    }
+
+    public function show(Category $category){
+        return new CategoryResource($category);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\CategoriesController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('categories',[ App\Http\Controllers\Api\CategoriesController::class,'index']);
+Route::get('categories',[ CategoriesController::class,'index']);
+Route::get('categories/{category}',[CategoriesController::class,'show']);
