346e2d68c7ed2e127ca03f10a2343331ca50c791
mitcool <dimitaraleksiev4@gmail.com>
2023-10-12 13:14:45 +0300

    All categories function

---
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoriesController extends Controller
+{
+    public function index(){
+        return Category::all();
+    }
+}
